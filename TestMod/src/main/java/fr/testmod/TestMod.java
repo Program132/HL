1a5a2762d2036c2be3ee3f77d871ed6a24c4c2fa
Program <67320703+Program132@users.noreply.github.com>
2026-01-14 21:14:36 +0100

    Added tests for chat + craft player events

--- TestMod/src/main/java/fr/testmod/TestMod.java
+++ TestMod/src/main/java/fr/testmod/TestMod.java
@@ -43,6 +43,18 @@ public class TestMod extends SimplePlugin {
                 .log("[TESTMOD] Damage: " + event.getPlayerName() + " took " + event.getDamage().getAmount());
     }
 
+    @EventHandler
+    public void onChat(fr.hytale.loader.event.types.PlayerChatEvent event) {
+        getLogger().at(Level.INFO)
+                .log("[TESTMOD] Chat: " + event.getSender().getUsername() + " says: " + event.getMessage());
+    }
+
+    @EventHandler
+    public void onCraft(fr.hytale.loader.event.types.PlayerCraftEvent event) {
+        getLogger().at(Level.INFO).log("[TESTMOD] Craft: " + event.getPlayerName()
+                + " crafted " + event.getQuantity() + "x " + event.getRecipeName());
+    }
+
     @fr.hytale.loader.command.Command(name = "hello", description = "Says hello")
     public void onHello(com.hypixel.hytale.server.core.command.system.CommandContext ctx) {
         ctx.sender().sendMessage(com.hypixel.hytale.server.core.Message.raw("Hello World!"));

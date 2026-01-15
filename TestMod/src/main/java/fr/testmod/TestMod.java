caa6c8a6a07a626336981dfedaaa35ef67c1c329
Program <67320703+Program132@users.noreply.github.com>
2026-01-15 14:27:57 +0100

    Update TestMod.java

--- TestMod/src/main/java/fr/testmod/TestMod.java
+++ TestMod/src/main/java/fr/testmod/TestMod.java
@@ -28,31 +28,72 @@ public class TestMod extends SimplePlugin {
     }
 
     @EventHandler
-    public void onJoin(fr.hytale.loader.event.types.PlayerJoinEvent event) {
+    public void onJoin(fr.hytale.loader.event.types.player.PlayerJoinEvent event) {
         getLogger().at(Level.INFO).log("[TESTMOD] Join: " + event.getPlayerName());
     }
 
     @EventHandler
-    public void onQuit(fr.hytale.loader.event.types.PlayerQuitEvent event) {
+    public void onQuit(fr.hytale.loader.event.types.player.PlayerQuitEvent event) {
         getLogger().at(Level.INFO).log("[TESTMOD] Quit: " + event.getPlayerName());
     }
 
     @EventHandler
-    public void onDamage(fr.hytale.loader.event.types.PlayerDamageEvent event) {
+    public void onDamage(fr.hytale.loader.event.types.player.PlayerDamageEvent event) {
         getLogger().at(Level.INFO)
                 .log("[TESTMOD] Damage: " + event.getPlayerName() + " took " + event.getDamage().getAmount());
     }
 
     @EventHandler
-    public void onChat(fr.hytale.loader.event.types.PlayerChatEvent event) {
+    public void onChat(fr.hytale.loader.event.types.player.PlayerChatEvent event) {
         getLogger().at(Level.INFO)
                 .log("[TESTMOD] Chat: " + event.getSender().getUsername() + " says: " + event.getMessage());
     }
 
     @EventHandler
-    public void onCraft(fr.hytale.loader.event.types.PlayerCraftEvent event) {
+    public void onCraft(fr.hytale.loader.event.types.player.PlayerCraftEvent event) {
         getLogger().at(Level.INFO).log("[TESTMOD] Craft: " + event.getPlayerName()
                 + " crafted " + event.getQuantity() + "x " + event.getRecipeName());
+    }
+
+    // ECS Events
+    @EventHandler
+    public void onBreakBlock(fr.hytale.loader.event.types.ecs.BreakBlockEvent event) {
+        getLogger().at(Level.INFO).log("[TESTMOD] BreakBlock: " + event.getBlockType().getId()
+                + " at " + event.getTargetBlock());
+    }
+
+    @EventHandler
+    public void onPlaceBlock(fr.hytale.loader.event.types.ecs.PlaceBlockEvent event) {
+        getLogger().at(Level.INFO).log("[TESTMOD] PlaceBlock: block at " + event.getTargetBlock());
+    }
+
+    @EventHandler
+    public void onUseBlock(fr.hytale.loader.event.types.ecs.UseBlockEvent event) {
+        getLogger().at(Level.INFO).log("[TESTMOD] UseBlock: " + event.getBlockType().getId()
+                + " at " + event.getTargetBlock() + " (interaction: " + event.getInteractionType() + ")");
+    }
+
+    @EventHandler
+    public void onDamageBlock(fr.hytale.loader.event.types.ecs.DamageBlockEvent event) {
+        getLogger().at(Level.INFO).log("[TESTMOD] DamageBlock: " + event.getBlockType().getId()
+                + " damage: " + event.getDamage() + " (current: " + event.getCurrentDamage() + ")");
+    }
+
+    @EventHandler
+    public void onDropItem(fr.hytale.loader.event.types.ecs.DropItemEvent event) {
+        getLogger().at(Level.INFO).log("[TESTMOD] DropItem: " + event.getItemStack().getItem().getId()
+                + " x" + event.getItemStack().getQuantity());
+    }
+
+    @EventHandler
+    public void onDiscoverZone(fr.hytale.loader.event.types.ecs.DiscoverZoneEvent event) {
+        getLogger().at(Level.INFO).log("[TESTMOD] DiscoverZone: " + event.getDiscoveryInfo());
+    }
+
+    @EventHandler
+    public void onCraftRecipe(fr.hytale.loader.event.types.ecs.CraftRecipeEvent event) {
+        getLogger().at(Level.INFO).log("[TESTMOD] CraftRecipe: " + event.getRecipeName()
+                + " x" + event.getQuantity());
     }
 
     @fr.hytale.loader.command.Command(name = "hello", description = "Says hello")

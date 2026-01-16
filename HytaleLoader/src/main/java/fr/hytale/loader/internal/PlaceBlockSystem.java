55902535c81a859fdbe879755229ad49c60e2c44
Program <67320703+Program132@users.noreply.github.com>
2026-01-16 16:42:43 +0100

    Create PlaceBlockSystem.java

---
+++ HytaleLoader/src/main/java/fr/hytale/loader/internal/PlaceBlockSystem.java
@@ -0,0 +1,55 @@
+package fr.hytale.loader.internal;
+
+import com.hypixel.hytale.component.ArchetypeChunk;
+import com.hypixel.hytale.component.CommandBuffer;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.component.query.Query;
+import com.hypixel.hytale.component.system.EntityEventSystem;
+import com.hypixel.hytale.server.core.HytaleServer;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.event.events.ecs.PlaceBlockEvent;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Internal ECS system for handling block place events.
+ */
+public class PlaceBlockSystem extends EntityEventSystem<EntityStore, PlaceBlockEvent> {
+
+    public PlaceBlockSystem() {
+        super(PlaceBlockEvent.class);
+    }
+
+    @Override
+    public Query<EntityStore> getQuery() {
+        return Query.any();
+    }
+
+    @Override
+    public void handle(
+            int entityIndex,
+            @Nonnull ArchetypeChunk<EntityStore> chunk,
+            @Nonnull Store<EntityStore> store,
+            @Nonnull CommandBuffer<EntityStore> commandBuffer,
+            @Nonnull PlaceBlockEvent event) {
+
+        fr.hytale.loader.api.Player playerWrapper = null;
+
+        Player nativePlayer = chunk.getComponent(entityIndex, Player.getComponentType());
+        if (nativePlayer != null) {
+            PlayerRef playerRef = chunk.getComponent(entityIndex, PlayerRef.getComponentType());
+            if (playerRef != null) {
+                playerWrapper = new fr.hytale.loader.api.Player(nativePlayer, playerRef);
+            }
+        }
+
+        fr.hytale.loader.event.types.ecs.PlaceBlockEvent newEvent =
+                new fr.hytale.loader.event.types.ecs.PlaceBlockEvent(event, playerWrapper);
+
+        HytaleServer.get().getEventBus()
+                .dispatchFor(fr.hytale.loader.event.types.ecs.PlaceBlockEvent.class, null)
+                .dispatch(newEvent);
+    }
+}

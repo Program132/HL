6ddcca32f5bd7943b45b12c236a8c41c9211ec1c
Program <67320703+Program132@users.noreply.github.com>
2026-01-16 16:42:22 +0100

    Create CraftRecipeSystem.java

---
+++ HytaleLoader/src/main/java/fr/hytale/loader/internal/CraftRecipeSystem.java
@@ -0,0 +1,55 @@
+package fr.hytale.loader.internal;
+
+import com.hypixel.hytale.component.ArchetypeChunk;
+import com.hypixel.hytale.component.CommandBuffer;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.component.query.Query;
+import com.hypixel.hytale.component.system.EntityEventSystem;
+import com.hypixel.hytale.server.core.HytaleServer;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.event.events.ecs.CraftRecipeEvent;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Internal ECS system for handling crafting recipe events.
+ */
+public class CraftRecipeSystem extends EntityEventSystem<EntityStore, CraftRecipeEvent.Pre> {
+
+    public CraftRecipeSystem() {
+        super(CraftRecipeEvent.Pre.class);
+    }
+
+    @Override
+    public Query<EntityStore> getQuery() {
+        return Query.any();
+    }
+
+    @Override
+    public void handle(
+            int entityIndex,
+            @Nonnull ArchetypeChunk<EntityStore> chunk,
+            @Nonnull Store<EntityStore> store,
+            @Nonnull CommandBuffer<EntityStore> commandBuffer,
+            @Nonnull CraftRecipeEvent.Pre event) {
+
+        fr.hytale.loader.api.Player playerWrapper = null;
+
+        Player nativePlayer = chunk.getComponent(entityIndex, Player.getComponentType());
+        if (nativePlayer != null) {
+            PlayerRef playerRef = chunk.getComponent(entityIndex, PlayerRef.getComponentType());
+            if (playerRef != null) {
+                playerWrapper = new fr.hytale.loader.api.Player(nativePlayer, playerRef);
+            }
+        }
+
+        fr.hytale.loader.event.types.ecs.CraftRecipeEvent newEvent =
+                new fr.hytale.loader.event.types.ecs.CraftRecipeEvent(event, playerWrapper);
+
+        HytaleServer.get().getEventBus()
+                .dispatchFor(fr.hytale.loader.event.types.ecs.CraftRecipeEvent.class, null)
+                .dispatch(newEvent);
+    }
+}
